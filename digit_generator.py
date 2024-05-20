3bdbc5e44474735523de9b5afcba7b6c3faab43d
Flychuban <kaloanas07@gmail.com>
2024-05-20 16:29:13 +0300

    add generator model structure

--- digit_generator.py
+++ digit_generator.py
@@ -1,6 +1,7 @@
 import tensorflow as tf
 from keras.layers import Dense, Flatten, Reshape, Conv2D, Conv2DTranspose, LeakyReLU, BatchNormalization, Dropout
 from matplotlib import pyplot as plt
+from keras.models import Sequential
 
 def load_preprocess_data():
     # Load the MNIST dataset
@@ -15,5 +16,30 @@ def load_preprocess_data():
     train_dataset = tf.data.Dataset.from_tensor_slices(x_train).shuffle(BUFFER_SIZE).batch(BATCH_SIZE)
     return train_dataset, x_train, y_train, x_test, y_test
 
+def generator_model():
+    model = Sequential()
+    model.add(Dense(7*7*256, use_bias=False, input_shape=(100,)))
+    model.add(BatchNormalization()) # Normalize and scale inputs or activations
+    model.add(LeakyReLU())
+    
+    model.add(Reshape((7, 7, 256)))
+    assert model.output_shape == (None, 7, 7, 256) # Note: None is the batch size
+    
+    model.add(Conv2DTranspose(128, (5, 5), strides=(1, 1), padding='same', use_bias=False))
+    assert model.output_shape == (None, 7, 7, 128)
+    model.add(BatchNormalization())
+    model.add(LeakyReLU())
+    
+    model.add(Conv2DTranspose(64, (5, 5), strides=(2, 2), padding='same', use_bias=False))
+    assert model.output_shape == (None, 14, 14, 64)
+    model.add(BatchNormalization())
+    model.add(LeakyReLU())
+    
+    model.add(Conv2DTranspose(1, (5, 5), strides=(2, 2), padding='same', use_bias=False, activation='tanh'))
+    assert model.output_shape == (None, 28, 28, 1)
+    return model
 
+if __name__ == '__main__':
+    generator_model = generator_model()
+    generator_model.summary()
 

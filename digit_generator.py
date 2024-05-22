4d3455d40d06b6a3bf96486df206b58d9d3dfab8
Flychuban <kaloanas07@gmail.com>
2024-05-22 17:16:09 +0300

    Add training function for models

--- digit_generator.py
+++ digit_generator.py
@@ -2,6 +2,13 @@ import tensorflow as tf
 from keras.layers import Dense, Flatten, Reshape, Conv2D, Conv2DTranspose, LeakyReLU, BatchNormalization, Dropout
 from matplotlib import pyplot as plt
 from keras.models import Sequential
+import os
+
+EPOCHS = 50
+BATCH_SIZE = 256
+noise_dim = 100
+num_examples_to_generate = 16
+seed = tf.random.normal([num_examples_to_generate, noise_dim])
 
 def load_preprocess_data():
     # Load the MNIST dataset
@@ -10,7 +17,6 @@ def load_preprocess_data():
     x_train = (x_train - 127.5) / 127.5 #  Normalize the images to [-1, 1]
 
     BUFFER_SIZE = 60000
-    BATCH_SIZE = 256
 
     # Batch and shuffle the data
     train_dataset = tf.data.Dataset.from_tensor_slices(x_train).shuffle(BUFFER_SIZE).batch(BATCH_SIZE)
@@ -53,6 +59,49 @@ def discriminator_model():
     model.add(Dense(1, activation='sigmoid'))
     
     return model
+
+def train_models_func(): 
+
+    def discriminator_loss(real_output, fake_output):
+        real_loss = cross_entropy(tf.ones_like(real_output), real_output)
+        fake_loss = cross_entropy(tf.zeros_like(fake_output), fake_output)
+        total_loss = real_loss + fake_loss
+        return total_loss
+
+    def generator_loss(fake_output):
+        return cross_entropy(tf.ones_like(fake_output), fake_output)
+
+    cross_entropy = tf.keras.losses.BinaryCrossentropy(from_logits=True)
+    generator_optimizer = tf.keras.optimizers.Adam(1e-4)
+    discriminator_optimizer = tf.keras.optimizers.Adam(1e-4)
+    
+    checkpoint_dir = './training_checkpoints'
+    checkpoint_prefix = os.path.join(checkpoint_dir, "ckpt")
+    
+    checkpoint = tf.train.Checkpoint(generator_optimizer=generator_optimizer,
+                                     discriminator_optimizer=discriminator_optimizer,
+                                     generator=generator_model(),
+                                     discriminator=discriminator_model())
+    
+    @tf.function
+    def train_step(images):
+        noise = tf.random.normal([BATCH_SIZE, noise_dim])
+        
+        with tf.GradientTape() as gen_tape, tf.GradientTape() as disc_tape:
+            generated_images = generator_model(noise, training=True)
+            
+            real_output = discriminator_model(images, training=True)
+            fake_output = discriminator_model(generated_images, training=True)
+            
+            gen_loss = generator_loss(fake_output)
+            disc_loss = discriminator_loss(real_output, fake_output)
+            
+        gradients_of_generator = gen_tape.gradient(gen_loss, generator_model.trainable_variables)
+        gradients_of_discriminator = disc_tape.gradient(disc_loss, discriminator_model.trainable_variables)
+        
+        generator_optimizer.apply_gradients(zip(gradients_of_generator, generator_model.trainable_variables))
+        discriminator_optimizer.apply_gradients(zip(gradients_of_discriminator, discriminator_model.trainable_variables))
+
 
 if __name__ == '__main__':
     generator_model = generator_model()
